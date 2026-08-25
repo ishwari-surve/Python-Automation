72a289fc4d0ca0002185c41dab973da20533ab7a
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-25 22:37:08 +0530

    Add command-line arguments iteration utility

--- Command-line-arguments/CommandLine3.py
+++ Command-line-arguments/CommandLine3.py
@@ -4,7 +4,7 @@ def main():
     print("Command Line Arguments are: ")
     for i in range(len(sys.argv)):
         print(sys.argv[i])      
-    print(len(sys.argv))
+    print(len(sys.argv)) 
     
 if __name__=="__main__":
     main()
