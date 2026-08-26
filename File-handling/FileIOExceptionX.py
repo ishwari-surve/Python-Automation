16806fba596e48c6bca85249afe2fad23ecccb64
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-26 08:49:52 +0530

    Add file read mode with exception handling

---
+++ File-handling/FileIOExceptionX.py
@@ -0,0 +1,15 @@
+def main():
+    try:
+        open("Demo.txt","r")
+        print("Filesngets successfully opened")
+    except FileNotFoundError:
+        print("Unable to oprn file as there is no such file")
+
+    
+    finally:
+        print("End of application")
+
+
+
+if __name__=="__main__":
+    main()
