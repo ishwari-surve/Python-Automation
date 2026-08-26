d6490aa50416b9d6aea3103beea329ef4157997f
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-26 09:07:52 +0530

    Add files via upload

---
+++ File-handling/FileOperationReadX.py
@@ -0,0 +1,25 @@
+def main():
+    fobj = None
+
+    try:
+        fobj=open("Hello.txt","r")
+        print("Filesngets successfully opened")
+
+        Data = fobj.read(6)
+
+        print("Data from file is:",Data)
+
+        fobj.close()
+
+    except FileNotFoundError:
+        print("Unable to oprn file as there is no such file")
+
+    
+    finally:
+        print("End of application")
+        
+
+
+
+if __name__=="__main__":
+    main()
