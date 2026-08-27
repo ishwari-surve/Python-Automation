ebbddb27a9a711d7aa93b502247161ca58e59f76
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-27 10:59:54 +0530

    Add files via upload

---
+++ Directory-automation/DirectoryScan3.py
@@ -0,0 +1,34 @@
+import os
+
+def DirectoryScanner(DirectoryName="Marvellous"):
+
+    Ret = os.path.exists(DirectoryName)
+
+    if(Ret == False):
+        print("there is no such directory")
+        return
+    
+    
+    
+    print("Contents of the directory are:")
+
+    for FolderName,SubFolderName,FileName in os.walk(DirectoryName):
+        print("Folder name:",FolderName)
+
+        for subf in SubFolderName:
+            print("SubFolder name:",subf)
+        
+        for fname in FileName:
+            print("File name:",fname)
+
+
+def main():
+    DirectoryName = input("Entr the name of directory")
+
+    DirectoryScanner(DirectoryName)
+
+  
+
+    
+if __name__=="__main__":
+    main()
