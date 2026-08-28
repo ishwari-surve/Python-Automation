e109f9b7941455549aeb242423b271daf4d96b5b
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-28 08:38:24 +0530

    Add scheduled directory automation with enhanced logging

---
+++ Directory-automation/Ishwari.py
@@ -0,0 +1,82 @@
+import sys
+import os
+import time
+import schedule
+
+
+def DirectoryScanner(DirName="Marvellous"):
+    Border="-"*50
+    timestamp=time.ctime()
+
+    Logfilename="Marvellous%s.log" %(timestamp)
+    Logfilename=Logfilename.replace(" ","_")
+    Logfilename=Logfilename.replace(":","_")
+    fobj=open(Logfilename,"w")
+    fobj.write((Border+"\n"))
+    fobj.write("This is a log file created by Marvellous Infosystem\n")
+    fobj.write("This is a directory cleaner script\n")
+    fobj.write((Border+"\n"))
+
+    Ret=False
+
+    Ret=os.path.exists(DirName)
+    if(Ret==False):
+        print("There is such directory")
+        return
+    
+    Ret=os.path.isdir(DirName)
+    if (Ret==False):
+        print("It is not a directory")
+        return
+
+    FileCount=0
+    EmptyFileCount=0
+
+    for FolderName, SubFolder, FileName in os.walk(DirName):
+         
+        for fname in FileName:
+            FileCount=FileCount+1
+
+            fname=os.path.join(FolderName,fname)
+            
+            if(os.path.getsize(fname)==0):
+                EmptyFileCount=EmptyFileCount+1
+                os.remove(fname)    
+
+    
+    fobj.write("Total file scanned: " + str(FileCount) + "\n")
+
+    fobj.write("Total Empty Files Count:"+str(EmptyFileCount)+"\n")
+    fobj.write("This log file is created at: "+timestamp+"\n")
+    fobj.write(Border+"\n")
+
+    fobj.close()
+
+def main():
+    Border="-"*50
+    print(Border)
+    print("----------Marvellous Directory Automation---------")
+    print(Border)
+
+    if(len(sys.argv)!=2):
+        print("Invalid number of arguements")
+        print("Please specify the name of directory")
+        return
+
+    #DirectoryScanner(sys.argv[1])
+    schedule.every(1).minute.do(DirectoryScanner)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+
+
+    Border="-"*50
+    print(Border)
+    print("----------Marvellous Directory Automation---------")
+    print(Border)
+    
+if __name__=="__main__":
+    main()
